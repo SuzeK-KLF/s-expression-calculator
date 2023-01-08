9d1580ba5de13422ab63b08ecebafae858253c21
suzek-klf <suzeklf@gmail.com>
2023-01-07 21:05:00 -0500

    finished

--- hello.py
+++ hello.py
@@ -1,28 +1,55 @@
 import sys
 
-def add(n1, n2):
-    answer = n1+n2
-    print(answer)
+class SExpression:
+    # prepare a container to store computed values
+    precalculate = {}
 
-# def calculate(func, exp1, exp2):
-#     result = 0
-    
+    def calculate(self, expression):
+        while ')' in expression:
+
+            if expression in self.precalculate:
+                return self.precalculate[expression]
+
+            right_bound = expression.index(')')
+            # find the last occurrence '(' in first function ')'
+            left_bound = expression[:right_bound].rindex('(')
+
+            value = self.calculateAtom(expression[left_bound + 1:right_bound])
+
+            if left_bound == 0:
+                return value
+
+            else:
+                expression = expression[:left_bound] + str(value) + expression[right_bound+1:]
+
+        return int(expression)
+
+    def calculateAtom(self, expression):
+        if expression in self.precalculate:
+            return self.precalculate[expression]
+
+        pieces = expression.split()
+
+        if pieces[0] == 'add':
+            answer = int(pieces[1]) + int(pieces[2])
+
+        elif pieces[0] == 'multiply':
+            answer = int(pieces[1]) * int(pieces[2])
+
+        else:
+            answer = int(expression)
+
+        # Add to pre calculated dict
+        self.precalculate[expression] = answer
+        return answer
 
 
 def main():
     if len(sys.argv) <=2:
         print("Please give enough info")
     else:
-        function = sys.argv[1]
-        print(function)
-        print(len(sys.argv))
-        if function == "add":
-            if len(sys.argv) <4:
-                print(sys.argv[2])
-            else:
-                num1 = int(sys.argv[2])
-                num2 = int(sys.argv[3])
-                add(num1, num2)
+        print(SExpression().calculate(sys.argv[1]))
+
 
 if ( __name__ == "__main__"):
     main()
